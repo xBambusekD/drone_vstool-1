b3d714a9c9ea5a52b3d13905892744a228c92bdf
Hubinatorr <robert.hubinak@gmail.com>
2020-07-01 17:13:57 +0200

    Updated Structure
    Updated Structure for Mapbox and Rossharp

---
+++ VSTool/Assets/RosSubscribers/CompassSubscriber.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RosSharp.RosBridgeClient.Messages;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class CompassSubscriber : Subscriber<Messages.Float64>
+    {
+        public float orientation;
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        protected override void ReceiveMessage(Messages.Float64 position)
+        {
+            orientation = position.data;
+            //Debug.Log("compass: "+position.data);
+        }
+    }
+}
---
+++ VSTool/Assets/RosSubscribers/ImuSubscriber.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RosSharp.RosBridgeClient.Messages;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class ImuSubscriber : Subscriber<Messages.Imu>
+    {
+        public Messages.Imu imuData;
+
+        private bool isMessageReceived;
+        private MyRosConnector droneRosConnector;
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        protected override void ReceiveMessage(Messages.Imu data)
+        {
+            imuData = data;
+            isMessageReceived = false;
+        }
+
+        private void Update()
+        {
+            if (isMessageReceived)
+            {
+                isMessageReceived=false;
+                if (droneRosConnector != null) droneRosConnector.RecievedMessageSetTime(); //musi se volat z updatu, jinak neni definovany cas
+            }
+        }
+    }
+}
