f467919722cf1c8b033345e6cb0bbbcfbefcab0e
xBambusekD <bambusek7619@gmail.com>
2023-10-31 13:39:24 +0100

    feat: Highlighter asset added as a submodule

--- scripts/link_submodules.bat
+++ scripts/link_submodules.bat
@@ -1,5 +1,6 @@
 md ..\DroCo\Assets\Submodules
 
 mklink /j ..\DroCo\Assets\Submodules\GStreamerUnity ..\DroCo\Submodules\mrayGStreamerUnity\Unity\UnityTests\Assets
+mklink /j ..\DroCo\Assets\Submodules\Highlighters-Outlines ..\DroCo\Submodules\droco_private\3rdparty\Highlighters-Outlines
 
 pause
