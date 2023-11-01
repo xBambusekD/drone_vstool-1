39f3351ba2f890c063e14d84fc5f310c41c217f0
xBambusekD <bambusek7619@gmail.com>
2023-11-01 16:54:41 +0100

    fix: missing submodule for outlines added

--- scripts/link_submodules.bat
+++ scripts/link_submodules.bat
@@ -4,5 +4,6 @@ mklink /j ..\DroCo\Assets\Submodules\GStreamerUnity ..\DroCo\Submodules\mrayGStr
 mklink /j ..\DroCo\Assets\Submodules\Highlighters-Outlines ..\DroCo\Submodules\droco_private\3rdparty\Highlighters-Outlines
 mklink /j ..\DroCo\Assets\Submodules\ArcGIS-Samples ..\DroCo\Submodules\droco_private\3rdparty\ArcGIS-Samples
 mklink /j "..\DroCo\Assets\Submodules\Modern UI Pack" "..\DroCo\Submodules\droco_private\3rdparty\Modern UI Pack"
+mklink /j ..\DroCo\Assets\Submodules\Unity-URP-Outlines ..\DroCo\Submodules\droco_private\3rdparty\Unity-URP-Outlines
 
 pause
