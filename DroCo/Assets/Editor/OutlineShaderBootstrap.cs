2cedc28bafd2a558bc1a91dd6b8888e2c540845a
xBambusekD <bambusek7619@gmail.com>
2025-11-10 15:11:00 +0100

    fix: shader reimport minor fix

--- DroCo/Assets/Editor/OutlineShaderBootstrap.cs
+++ DroCo/Assets/Editor/OutlineShaderBootstrap.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 [InitializeOnLoad]
 public static class OutlineShaderBootstrap {
     static OutlineShaderBootstrap() {
-        TryReimportShader("Assets/Submodules/Unity-URP-Outlines/Outlines/ShaderGraphs/ViewSpaceNormals.shader");
-        TryReimportShader("Assets/Submodules/Unity-URP-Outlines/Outlines/ShaderGraphs/Outlines.shader");
-        TryReimportShader("Assets/Submodules/Unity-URP-Outlines/Outlines/ShaderGraphs/UnlitColor.shader");
+        TryReimportShader("Assets/Submodules/Unity-URP-Outlines/Outlines/ShaderGraphs/ViewSpaceNormals.shadergraph");
+        TryReimportShader("Assets/Submodules/Unity-URP-Outlines/Outlines/ShaderGraphs/Outlines.shadergraph");
+        TryReimportShader("Assets/Submodules/Unity-URP-Outlines/Outlines/ShaderGraphs/UnlitColor.shadergraph");
     }
 
     private static void TryReimportShader(string path) {
-        var shader = Shader.Find(System.IO.Path.GetFileNameWithoutExtension(path));
+        var shader = Shader.Find("Hidden/" + System.IO.Path.GetFileNameWithoutExtension(path));
         if (shader == null && System.IO.File.Exists(path)) {
             Debug.Log($"[Bootstrap] Reimporting {path} so URP features can find it...");
             AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
