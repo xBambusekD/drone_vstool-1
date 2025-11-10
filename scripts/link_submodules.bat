3c762a4f5343f74a2e4173d585374ac4d7a2dfee
xBambusekD <bambusek7619@gmail.com>
2025-11-10 11:18:05 +0100

    reimported outlines private asset in newer unity version

--- scripts/link_submodules.bat
+++ scripts/link_submodules.bat
@@ -1,7 +1,7 @@
 md ..\DroCo\Assets\Submodules
 
 mklink /j ..\DroCo\Assets\Submodules\GStreamerUnity ..\DroCo\Submodules\mrayGStreamerUnity\Unity\UnityTests\Assets
-mklink /j ..\DroCo\Assets\Submodules\Highlighters-Outlines ..\DroCo\Submodules\droco_private\3rdparty\Highlighters-Outlines
+mklink /j ..\DroCo\Assets\Submodules\Ultimate-Outlines ..\DroCo\Submodules\droco_private\3rdparty\Ultimate-Outlines
 mklink /j ..\DroCo\Assets\Submodules\ArcGIS-Samples ..\DroCo\Submodules\droco_private\3rdparty\ArcGIS-Samples
 mklink /j ..\DroCo\Assets\Submodules\ArcGIS ..\DroCo\Submodules\droco_private\3rdparty\ArcGIS
 mklink /j "..\DroCo\Assets\Submodules\Modern UI Pack" "..\DroCo\Submodules\droco_private\3rdparty\Modern UI Pack"
