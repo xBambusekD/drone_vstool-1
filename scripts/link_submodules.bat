3e9e68af21d919c9912464c9611ada7a8dbe669d
xBambusekD <bambusek7619@gmail.com>
2025-11-13 14:33:56 +0100

    [feat] arcgis updated to version 2.1.0

--- scripts/link_submodules.bat
+++ scripts/link_submodules.bat
@@ -2,7 +2,6 @@ md ..\DroCo\Assets\Submodules
 
 mklink /j ..\DroCo\Assets\Submodules\GStreamerUnity ..\DroCo\Submodules\mrayGStreamerUnity\Unity\UnityTests\Assets
 mklink /j ..\DroCo\Assets\Submodules\Ultimate-Outlines ..\DroCo\Submodules\droco_private\3rdparty\Ultimate-Outlines
-mklink /j ..\DroCo\Assets\Submodules\ArcGIS-Samples ..\DroCo\Submodules\droco_private\3rdparty\ArcGIS-Samples
 mklink /j ..\DroCo\Assets\Submodules\ArcGIS ..\DroCo\Submodules\droco_private\3rdparty\ArcGIS
 mklink /j "..\DroCo\Assets\Submodules\Modern UI Pack" "..\DroCo\Submodules\droco_private\3rdparty\Modern UI Pack"
 mklink /j ..\DroCo\Assets\Submodules\Unity-URP-Outlines ..\DroCo\Submodules\droco_private\3rdparty\Unity-URP-Outlines
